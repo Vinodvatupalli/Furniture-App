8e16e27fe536ff2010d401d5bafc1f804c211d28
Vinod Vatupalli <vinodvatupalli869@gmail.com>
2022-07-01 23:01:25 +0530

    added file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonCard,IonImg, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+// import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonImg className="furniture-image" src="/assets/images/img1.jpg"></IonImg><br/><br/>
+      <h1 className="intro">
+          Enjoy Your Online Shopping &emsp;&emsp;&emsp;&ensp;&nbsp;
+          </h1>
+        <div  className="main">
+          <br/><br/>
+          <div className="subcon">
+            Browse through all categories and shop the best furniture for your needs.
+          </div><br/><br/>
+          <IonButton
+          routerLink='/signup'
+          className='getstarted-btn' color="light-green" shape="round">Get Started</IonButton>
+
+        </div>
+
+
+        
       </IonContent>
     </IonPage>
   );
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,51 @@
+import {
+  IonButton,
+  IonCard,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonInput,
+  IonLabel,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import {logoFacebook,logoGoogle,logoTwitter} from 'ionicons/icons';
+import "./SignUp.css";
+// import '../assets/icon/img2.jpg';
+const SignUp: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent className="signup-main">
+        <IonImg className="signup-image" src="/assets/images/img2.jpg"></IonImg>
+        <div className="signup-head">
+          SignUp
+        </div><br/><br/>
+        <div className="signup-fields">
+         <IonInput className="signup-name" placeholder="Enter Name"></IonInput> 
+        <IonInput className="signup-email" placeholder="Enter Email"></IonInput>
+        <IonInput className="signup-password" placeholder="Enter Password"></IonInput>
+        </div>
+        <IonButton className="register-btn" color="light-green" >Register</IonButton>
+        
+          <IonLabel className="text">Already have an Account? &nbsp;<u className="signin-link">Sign In</u></IonLabel>
+          <IonLabel className="or">OR</IonLabel>
+          <div className="icons">
+          <IonIcon style={{fontSize:"20px" ,color:"primary" }}
+                  icon={logoFacebook}/>&emsp;
+         <IonIcon style={{fontSize:"20px" ,color:"primary" }}
+                  icon={logoGoogle}/>&emsp;
+         <IonIcon style={{fontSize:"20px" ,color:"primary" }}
+                  icon={logoTwitter}/>&emsp;
+          </div>
+        
+
+        
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default SignUp;
