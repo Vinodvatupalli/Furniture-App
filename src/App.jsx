2109608c73b2abf0a0d1ae346bf53461975a5eb1
Vinod Vatupalli <vinodvatupalli869@gmail.com>
2022-07-12 18:59:53 +0530

    added the appupdate functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonApp, IonRouterOutlet, setupIonicReact , isPlatform,useIonToast,useIonAlert} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Dashboard from './pages/Dashboard';
 import { AuthContextProvider } from "./context/AuthContext";
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -25,11 +26,110 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { App as app } from "@capacitor/app";
+import { Browser } from "@capacitor/browser";
+import { useEffect, useState } from "react";
+import { collection, doc, getDoc, setDoc } from "firebase/firestore"; 
+import { db } from "./pages/firebaseConfig";
 // import Dashboard from './pages/Dashboard';
 
 setupIonicReact();
 
-const App = () => (
+const App = () => {
+
+
+  const [updateDetails, setUpdateDetails] = useState({});
+  const [appVersion, setAppVersion] = useState("");
+
+  const updateRef = doc(db, "furniture-app-config","rfXJKCIZMPPVIlhm3xCO");
+  
+  const [presentAlert] = useIonAlert();
+  const [present] = useIonToast();
+
+  const handleToast = (msg) => {
+    present({
+      message: msg,
+      position: "top",
+      animated: true,
+      duration: 2000,
+      color: "dark3",
+      mode: "ios",
+    });
+  };
+
+  const handleAlert = (msg, title, btn, appVersion) => {
+    presentAlert({
+      header: title,
+      subHeader: `Version: ${appVersion}`,
+      message: msg,
+      buttons: [
+        {
+          text: btn,
+          role: "Download",
+          handler: async () => {
+            handleToast("Download Clicked");
+            await Browser.open({
+              url: "https://play.google.com/store/apps/details?id=com.furnitureapp.app",
+            });
+          },
+        },
+      ],
+      backdropDismiss: true,
+      translucent: true,
+      animated: true,
+      cssClass: "lp-sp-alert",
+    });
+  };
+
+  const getAppInfo = async () => {
+    let info = await app.getInfo();
+    return info;
+  };
+
+  const getConfigData = async () => {
+    const docSnap = await getDoc(updateRef);
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+      console.log("Document data:", docSnap.data());
+      setUpdateDetails(data.updateMsg);
+      setAppVersion(data.current);
+    } else {
+      console.log("No such document!");
+    }
+  };
+
+  const checkUpdate = async () => {
+    try {
+      if (isPlatform("android")) {
+        const currentAppInfo = getAppInfo();
+        if (appVersion > (await currentAppInfo).version) {
+          const msg = updateDetails.msg;
+          const title = updateDetails.title;
+          const btn = updateDetails.btn;
+          handleAlert(msg, title, btn, appVersion);
+        }
+      } 
+      // else {
+      //   const msg = "App is not running on android platform";
+      //   handleToast(msg);
+      // }
+    } 
+    catch (error) {
+      // handleAlert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getConfigData();
+    if (isPlatform("android")){
+      getAppInfo();
+    }
+  }, [0]);
+
+    checkUpdate();
+
+    return(
+      <>
   <IonApp>
     <AuthContextProvider>
       <IonReactRouter>
@@ -55,6 +155,8 @@ const App = () => (
       </IonReactRouter>
     </AuthContextProvider>
   </IonApp>
+  </>
 );
+}
 
 export default App;
