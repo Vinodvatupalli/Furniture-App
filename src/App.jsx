2e993deb5cc192d2adc5a9a95b1746f9e6f628fd
Vinod Vatupalli <vinodvatupalli869@gmail.com>
2022-07-16 20:01:49 +0530

    modified small chandes

--- src/App.jsx
+++ src/App.jsx
@@ -156,7 +156,7 @@ const App = () => {
               </Route>
 
               <Route exact path="/">
-                <Redirect to="/dashboard" />
+                <Redirect to="/home" />
               </Route>
             </IonRouterOutlet>
           </IonReactRouter>
